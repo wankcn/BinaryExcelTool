912ea1ebaaf47c170a6307b9a224956c5872c580
wenruo <wankcn@icloud.com>
2023-01-16 18:38:52 +0800

    增加测试用例

---
+++ Assets/Scripts/Editor/ExcelReadTest.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using Excel;
+
+public class ExcelReadTest
+{
+    private static string EXCEL_PATH = System.Environment.CurrentDirectory + "/Excel/";
+
+
+    [MenuItem("Tools/LogDataPath")]
+    private static void LogDataPath()
+    {
+        Debug.Log($"System.Environment.CurrentDirectory - {System.Environment.CurrentDirectory}");
+        Debug.Log($"Application.dataPath - {Application.dataPath}");
+        Debug.Log($"Application.persistentDataPath - {Application.persistentDataPath}");
+    }
+
+
+    [MenuItem("Tools/OpenExcel")]
+    private static void OpenExcel()
+    {
+        using (FileStream fs = File.Open(EXCEL_PATH + "TestInfo.xlsx", FileMode.Open, FileAccess.Read))
+        {
+            // 文件流获取Excel数据
+            IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(fs);
+
+            // 将Excel表中的数据转为DataSet
+            DataSet result = excelReader.AsDataSet();
+
+            for (int i = 0; i < result.Tables.Count; i++)
+            {
+                Debug.Log($"表名 {result.Tables[i].TableName}");
+                Debug.Log($"行数 {result.Tables[i].Rows.Count}");
+                Debug.Log($"列数 {result.Tables[i].Columns.Count}");
+            }
+
+            fs.Close();
+        }
+    }
+
+    [MenuItem("Tools/ReadExcel")]
+    private static void ReadExcel()
+    {
+        using (FileStream fs = File.Open(EXCEL_PATH + "TestInfo.xlsx", FileMode.Open, FileAccess.Read))
+        {
+            IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(fs);
+            DataSet result = excelReader.AsDataSet();
+            DataTable table = result.Tables[0];
+            for (int i = 0; i < table.Rows.Count; i++)
+            {
+                Debug.Log($"----- 第 {i} 行 ------");
+                for (int j = 0; j < table.Columns.Count; j++)
+                {
+                    Debug.Log($"{table.Rows[i][j]}");
+                }
+            }
+
+            fs.Close();
+        }
+    }
+}
