c9adf59f3208b413ee2a3871c80ef8b603a63b77
wenruo <wankcn@icloud.com>
2023-01-16 20:20:51 +0800

    读取excel表获取Table

---
+++ Assets/Editor/ExcelTool/ExcelTool.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using Excel;
+using UnityEditor;
+using UnityEngine;
+
+public class ExcelTool
+{
+    /// Excel文件根目录
+    private static readonly string EXCEL_PATH = Environment.CurrentDirectory + "/Excel/";
+
+
+    [MenuItem("Tools/GenerateExcelInfo")]
+    private static void GenerateExcelInfo()
+    {
+        // 获得Excel中所有文件
+        DirectoryInfo dInfo = Directory.CreateDirectory(EXCEL_PATH);
+        FileInfo[] files = dInfo.GetFiles();
+        GenerateExcelInfo(files);
+    }
+
+
+    private static void GenerateExcelInfo(FileInfo[] files)
+    {
+        for (int i = 0; i < files.Length; i++)
+        {
+            // 非Excel文件不处理
+            if (!IsExcelFile(files[i].Name)) continue;
+
+            // 打开Excel得到表数据
+            DataTableCollection tableCollection;
+            using (FileStream fs = files[i].Open(FileMode.Open, FileAccess.Read))
+            {
+                // 得到Excel数据
+                IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(fs);
+                tableCollection = excelReader.AsDataSet().Tables;
+                fs.Close();
+            }
+
+            // 遍历表信息
+            foreach (DataTable table in tableCollection)
+            {
+                Debug.Log(table.TableName);
+                // 生成数据结构
+                // 生成容器
+                // 生成2进制
+            }
+        }
+    }
+
+
+    /// 检查是否是ExcelFile
+    private static bool IsExcelFile(string fullName)
+    {
+        return fullName.EndsWith(".csv", StringComparison.Ordinal)
+               || fullName.EndsWith(".xls", StringComparison.Ordinal)
+               || fullName.EndsWith(".xlsx", StringComparison.Ordinal)
+               || fullName.EndsWith(".xlsm", StringComparison.Ordinal);
+    }
+}
