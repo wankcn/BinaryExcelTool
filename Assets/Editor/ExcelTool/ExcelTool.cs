d9eb1c4a16a4691f9192e1b841c9725af3a171ac
wenruo <wankcn@icloud.com>
2023-01-17 11:22:10 +0800

    获取文件夹下包含子文件夹所有文件

--- Assets/Editor/ExcelTool/ExcelTool.cs
+++ Assets/Editor/ExcelTool/ExcelTool.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.IO;
@@ -16,23 +15,21 @@ public class ExcelTool
     [MenuItem("Tools/GenerateExcelInfo")]
     private static void GenerateExcelInfo()
     {
-        // 获得Excel中所有文件
-        DirectoryInfo dInfo = Directory.CreateDirectory(EXCEL_PATH);
-        FileInfo[] files = dInfo.GetFiles();
+        List<FileInfo> files = GetAllFiles(EXCEL_PATH);
         GenerateExcelInfo(files);
     }
 
 
-    private static void GenerateExcelInfo(FileInfo[] files)
+    private static void GenerateExcelInfo(List<FileInfo> files)
     {
-        for (int i = 0; i < files.Length; i++)
+        foreach (var fileInfo in files)
         {
             // 非Excel文件不处理
-            if (!IsExcelFile(files[i].Name)) continue;
+            if (!IsExcelFile(fileInfo.Name)) continue;
 
             // 打开Excel得到表数据
             DataTableCollection tableCollection;
-            using (FileStream fs = files[i].Open(FileMode.Open, FileAccess.Read))
+            using (FileStream fs = fileInfo.Open(FileMode.Open, FileAccess.Read))
             {
                 // 得到Excel数据
                 IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(fs);
@@ -60,5 +57,56 @@ public class ExcelTool
                || fullName.EndsWith(".xls", StringComparison.Ordinal)
                || fullName.EndsWith(".xlsx", StringComparison.Ordinal)
                || fullName.EndsWith(".xlsm", StringComparison.Ordinal);
+    }
+
+    /// 获得目录下所有文件包含所有子文件夹
+    private static List<FileInfo> GetAllFiles(string path)
+    {
+        List<FileInfo> files = new List<FileInfo>();
+
+        // 检查目录是否存在
+        if (!string.IsNullOrWhiteSpace(path))
+        {
+            if (Directory.Exists(path)) GetAllFilesOfDir(files, path);
+            else Directory.CreateDirectory(path);
+        }
+        else
+        {
+            Debug.Log($"{path}为空！！");
+        }
+
+        return files;
+    }
+
+    /// 递归获取指定类型文件,包含子文件夹
+    private static void GetAllFilesOfDir(List<FileInfo> files, string path)
+    {
+        try
+        {
+            // 文件夹列表   
+            string[] dir = Directory.GetDirectories(path);
+            DirectoryInfo dInfo = new DirectoryInfo(path);
+            FileInfo[] file = dInfo.GetFiles();
+
+            // 当前目录文件或文件夹不为空              
+            if (file.Length != 0 || dir.Length != 0)
+            {
+                // 显示当前目录所有文件   
+                foreach (FileInfo f in file)
+                {
+                    files.Add(f);
+                }
+
+                // 递归一下
+                foreach (string d in dir)
+                {
+                    GetAllFilesOfDir(files, d);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log($"获取路径{path}目录下的文件失败！！！错误信息{e.Message}");
+        }
     }
 }
--- Assets/Scripts/FilesTest.cs
+++ Assets/Scripts/FilesTest.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEngine;
 
 public class FilesTest : MonoBehaviour
@@ -9,78 +10,20 @@ public class FilesTest : MonoBehaviour
     private static readonly string EXCEL_PATH = Environment.CurrentDirectory + "/Assets/";
 
     // 文件列表
-    private static readonly List<FileInfo> m_filesList = new List<FileInfo>();
+    private static List<FileInfo> fileInfos = new List<FileInfo>();
 
 
     private void Start()
     {
-        GetAllFiles(EXCEL_PATH);
-        foreach (var n in m_filesList)
+        // fileInfos = GetAllFiles(EXCEL_PATH);
+        foreach (var n in fileInfos)
         {
             print(n.Name);
         }
     }
 
 
-    /// 获得目录下所有文件或指定文件类型文件(包含所有子文件夹)
-    public static List<FileInfo> GetAllFiles(string path)
-    {
-        //检查目录是否存在
-        if (!string.IsNullOrWhiteSpace(path))
-        {
-            if (Directory.Exists(path))
-            {
-                GetAllFilesOfDir(path);
-            }
-            else
-            {
-                Directory.CreateDirectory(path);
-            }
-        }
-        else
-        {
-            //注意这里的EverydayLog.Write()是我自定义的日志文件，可以根据需要保留或删除
-            Debug.Log("GetAllFileOfFolder/GetallFile()/存储视频文件的路径为空，请检查！！！");
-        }
-
-        return m_filesList;
-    }
-
-
-    /// 递归获取指定类型文件,包含子文件夹
-    private static void GetAllFilesOfDir(string path)
-    {
-        try
-        {
-            string[] dir = Directory.GetDirectories(path); //文件夹列表   
-            DirectoryInfo dInfo = new DirectoryInfo(path);
-            FileInfo[] file = dInfo.GetFiles();
-
-            if (file.Length != 0 || dir.Length != 0) //当前目录文件或文件夹不为空                   
-            {
-                foreach (FileInfo f in file) //显示当前目录所有文件   
-                {
-                    if (IsMetaFile(f.Name))
-                    {
-                        continue;
-                    }
-
-                    m_filesList.Add(f);
-                }
-
-                foreach (string d in dir)
-                {
-                    GetAllFilesOfDir(d); //递归   
-                }
-            }
-        }
-        catch (Exception ex)
-        {
-            //注意这里的EverydayLog.Write()是我自定义的日志文件，可以根据需要保留或删除
-            Debug.Log("/GetAllFileOfFolder()/GetAllFilesOfDir()/获取指定路径：" + path + "   下的文件失败！！！，错误信息=" +
-                      ex.Message);
-        }
-    }
+   
 
     /// 检查是否是是文件
     private static bool IsMetaFile(string fullName)
